cb46e053c825dae638baf5d5d22b9490f77b3fb4
luixvieira <henrique2.terceiro@gmail.com>
2024-03-08 22:35:27 -0300

    aula de variaveis

---
+++ aula_python.py
@@ -0,0 +1,23 @@
+Nome = 'Luis'
+Idade = 18
+Peso = 75
+Altura = 1.80
+usuario_python = True
+PCD = False
+data_de_nascimento = '31/08/2005'
+dia = 31
+mes = 8
+ano = 2005
+endereco = 'Rua lugar nenhum, 100'
+
+print(Nome)
+print(Idade)
+print(Peso)
+print(Altura)
+print(usuario_python)
+print(data_de_nascimento)
+print(dia,"/",mes,"/",ano)
+(endereco[3:5])
+
+type(data_de_nascimento)
+type(dia)
